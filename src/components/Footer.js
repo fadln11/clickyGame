b83541f4c64063bc0e98ae641105a79081c1cfb2
fadln11 <fadeline314@gmail.com>
2019-04-28 00:37:53 -0500

    Updates

--- src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Footer = props => (
-  <footer className="footer text-left" id="footer-container">
-    <div className="bottom">Clicky Game</div>
-  </footer>
-)
-
-export default Footer;
