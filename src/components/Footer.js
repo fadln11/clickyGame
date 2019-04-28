cadc2cd17bf6909685ce3a17528bb79e96ac4aa6
fadln11 <fadeline314@gmail.com>
2019-04-28 13:41:10 -0500

    Revert "Updates"
    This reverts commit b83541f4c64063bc0e98ae641105a79081c1cfb2.

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Footer = props => (
+  <footer className="footer text-left" id="footer-container">
+    <div className="bottom">Clicky Game</div>
+  </footer>
+)
+
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Header = props => (
+  <header className="header text-center" id="header-container">
+    <h1><b>Clicky Game</b></h1>
+    <h3 className="subtitle">Click on an image to earn points, but don't click on any more than once!</h3>
+  </header>
+)
+
+export default Header;
