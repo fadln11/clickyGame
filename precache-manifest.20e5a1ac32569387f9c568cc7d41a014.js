1ac636348434e13b865731fa51e7a8e855f17284
fadln11 <fadeline314@gmail.com>
2019-04-28 00:21:13 -0500

    Updates

---
+++ precache-manifest.20e5a1ac32569387f9c568cc7d41a014.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef672981f7314328c637bd5bcf3fd098",
+    "url": "/clickyGame/index.html"
+  },
+  {
+    "revision": "3e807dcc4797e35c5375",
+    "url": "/clickyGame/static/css/main.fbdf67f7.chunk.css"
+  },
+  {
+    "revision": "9634a2804d5597ac62b3",
+    "url": "/clickyGame/static/js/2.548e0644.chunk.js"
+  },
+  {
+    "revision": "3e807dcc4797e35c5375",
+    "url": "/clickyGame/static/js/main.33bf138f.chunk.js"
+  },
+  {
+    "revision": "95f5ce002aae55a30c31",
+    "url": "/clickyGame/static/js/runtime~main.d5f62368.js"
+  }
+]);
